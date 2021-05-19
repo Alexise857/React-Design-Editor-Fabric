881dbb8ecf9f7fbf557c1d637f5f224032b09b07
Alexis <alexise857@gmail.com>
2021-05-18 19:52:44 -0500

    feat: adding redo and undo and fixing UI details for navbar

--- src/components/Menu/MenuText.tsx
+++ src/components/Menu/MenuText.tsx
@@ -13,6 +13,7 @@ import {
   ButtonGroup,
   Box,
   MenuItem,
+  Stack
 } from "@chakra-ui/react"
 import { fontsList } from "@/constants/fonts"
 
@@ -214,71 +215,77 @@ function MenuText() {
             +
           </Box>
         </ButtonGroup>
-        <Box>
-          <div>
-            <div
-              style={{
-                cursor: "pointer",
-                position: "relative",
-                height: "30px",
-                width: "30px",
-                background: `${options.fill}`,
-                boxShadow: "inset 0 0 0 1px rgb(57 76 96 / 15%)",
-                borderRadius: "2px",
-              }}
-              onClick={() => handleClick()}
-            ></div>
 
-            {displayColorPicker ? (
-              <div style={{ ...popover }}>
-                <div style={cover} onClick={handleClose} />
-                <TwitterPicker
-                  onChange={(color) => handleChangeOption("fill", color.hex)}
-                />
-              </div>
+
+        <Stack spacing={2} direction="row" align="center">
+          <Box>
+            <div>
+              <div
+                style={{
+                  cursor: "pointer",
+                  position: "relative",
+                  height: "24px",
+                  width: "24px",
+                  background: `${options.fill}`,
+                  boxShadow: "inset 0 0 0 1px rgb(57 76 96 / 15%)",
+                  borderRadius: "2px",
+                }}
+                onClick={() => handleClick()}
+              ></div>
+
+              {displayColorPicker ? (
+                <div style={{ ...popover }}>
+                  <div style={cover} onClick={handleClose} />
+                  <TwitterPicker
+                    onChange={(color) => handleChangeOption("fill", color.hex)}
+                  />
+                </div>
+              ) : null}
+            </div>
+          </Box>
+          <div
+            style={{opacity: options.fontWeight !== "bold" ? "0.5" : "1"}}
+            onClick={() =>
+              handleChangeOption(
+                "fontWeight",
+                options.fontWeight == "normal" ? "bold" : "normal"
+              )
+            }
+          >
+            <BoldIcon />
+          </div>
+
+          <div
+            onClick={() =>
+              handleChangeOption(
+                "fontStyle",
+                options.fontStyle == "normal" ? "italic" : "normal"
+              )
+            }
+          >
+            <ItalicIcon />
+          </div>
+
+          <div
+            style={{
+              width: "1px",
+              height: "24px",
+              background: "rgba(57,76,96,.15)",
+            }}
+          />
+
+          <div onClick={() => changeTextAlign(options.textAlign)}>
+            {options.textAlign === "center" ? (
+              <TextAlignCenterIcon />
+            ) : options.textAlign === "left" ? (
+              <TextAlignLeftIcon />
+            ) : options.textAlign === "right" ? (
+              <TextAlignRightIcon />
             ) : null}
           </div>
-        </Box>
 
-        <div
-          onClick={() =>
-            handleChangeOption(
-              "fontWeight",
-              options.fontWeight == "normal" ? "bold" : "normal"
-            )
-          }
-        >
-          <BoldIcon />
-        </div>
+        </Stack>
 
-        <div
-          onClick={() =>
-            handleChangeOption(
-              "fontStyle",
-              options.fontStyle == "normal" ? "italic" : "normal"
-            )
-          }
-        >
-          <ItalicIcon />
-        </div>
-
-        <div
-          style={{
-            width: "1px",
-            height: "24px",
-            background: "rgba(57,76,96,.15)",
-          }}
-        />
-
-        <div onClick={() => changeTextAlign(options.textAlign)}>
-          {options.textAlign === "center" ? (
-            <TextAlignCenterIcon />
-          ) : options.textAlign === "left" ? (
-            <TextAlignLeftIcon />
-          ) : options.textAlign === "right" ? (
-            <TextAlignRightIcon />
-          ) : null}
-        </div>
       </ButtonGroup>
 
       <ButtonGroup spacing={2}></ButtonGroup>
@@ -312,7 +319,7 @@ function MenuText() {
         <div onClick={handleClone}>
           <DuplicateObjectIcon />
         </div>
-        <div style={{ color: "red" }} onClick={handleDelete}>
+        <div onClick={handleDelete}>
           <RemoveObjectIcon />
         </div>
       </div>
@@ -475,8 +482,9 @@ function BoldIcon() {
       viewBox="0 0 24 24"
     >
       <path
-        fill="red"
+        fill="black"
         fillRule="evenodd"
+        opacity="currentColor"
         d="M7.08 4.72h4.44c2.03 0 3.5.3 4.41.87.92.57 1.37 1.49 1.37 2.75 0 .85-.2 1.55-.6 2.1-.4.54-.93.87-1.6.98v.1c.91.2 1.56.58 1.96 1.13.4.56.6 1.3.6 2.2 0 1.31-.47 2.33-1.4 3.06A6.1 6.1 0 0 1 12.41 19H7.08V4.72zm3.03 5.66h1.75c.82 0 1.42-.13 1.79-.38.36-.26.55-.68.55-1.26 0-.55-.2-.94-.6-1.18a3.86 3.86 0 0 0-1.9-.36h-1.6v3.18zm0 2.4v3.72h1.97c.83 0 1.45-.16 1.84-.48.4-.32.6-.8.6-1.46 0-1.19-.85-1.78-2.54-1.78h-1.87z"
       ></path>
     </svg>
